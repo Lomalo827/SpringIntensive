d8d2d94508385db048ccc33294f5a145e7f8353a
Александр <stepushins@mail.ru>
2025-09-21 14:19:36 +0300

    InitialCommit

---
+++ src/main/java/com/Learn/Spring/Intensive/SpringIntensiveApplication.java
@@ -0,0 +1,13 @@
+package com.Learn.Spring.Intensive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntensiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntensiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Learn/Spring/Intensive/SpringIntensiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Learn.Spring.Intensive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntensiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
